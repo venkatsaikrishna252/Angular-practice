b376173ffb718d74487852da5ac6786cc7c50eca
saiKrishna <venkatsaikrishna252@gmail.com>
2023-11-27 19:15:28 -0500

    interface

--- src/app/student-details/student-details.component.ts
+++ src/app/student-details/student-details.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../student';
 
 @Component({
   selector: 'app-student-details',
   templateUrl: './student-details.component.html',
   styleUrls: ['./student-details.component.css']
 })
-export class StudentDetailsComponent {
+export class StudentDetailsComponent implements OnInit{
+
+
+  newStudent:Student={
+    name : "sai",
+    id: 0,
+    email: 'abc@gmail.com'
+  }
+
+  student:any={id:1,name:"john",age:25};
+  updatedStudent:any={...this.student,status:'major'};
+  
+  
 
   studentList:any[]=[
     {name:"sai",id:10,age:22},
@@ -14,25 +27,36 @@ export class StudentDetailsComponent {
   ]
 
   constructor(){}
-  ngOnInit():void{
-    this.studentList.forEach((student:any)=>{
-      // console.log(student);
-      if(student.age>=18){
-        // console.log("major");
-        student['status']="major";
-        
-      }else{
-        // console.log("minor");
-        student['status']="minor";
-        
-        
-      }
-    }
-    );
-    console.log(this.studentList);
-
+  ngOnInit(): void {
+    console.log(this.student);
+    console.log(this.updatedStudent);
+    console.log(this.newStudent);
+    
+    
   }
+  // ngOnInit(): void {
+  //   throw new Error('Method not implemented.');
+  // }
+  // ngOnInit():void{
+  //   this.studentList.forEach((student:any)=>{
+  //     // console.log(student);
+  //     if(student.age>=18){
+  //       // console.log("major");
+  //       student['status']="major";
+        
+  //     }else{
+  //       // console.log("minor");
+  //       student['status']="minor";
+        
+        
+  //     }
+  //   }
+  //   );
+  //   console.log(this.studentList);
 
+  // }
+
+ 
   
 
 }
