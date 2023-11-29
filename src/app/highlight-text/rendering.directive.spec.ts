d571005b06d4c993892f4f4b15c0347887d6a6d4
saiKrishna <venkatsaikrishna252@gmail.com>
2023-11-28 19:09:51 -0500

    hostlistener

--- src/app/highlight-text/rendering.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RenderingDirective } from './rendering.directive';
-
-describe('RenderingDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RenderingDirective();
-    expect(directive).toBeTruthy();
-  });
-});
